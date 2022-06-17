3976e9691aee14e826067f854425be4a14073151
Sarah Dylan <SarahmDylan@gmail.com>
2022-06-17 10:46:51 -0700

    Add functionality to digits, operators, and =
    Edited the updateNumberString function to handle most scenarios in
    regards to buttons pushes.

--- js/index.js
+++ js/index.js
@@ -1,3 +1,9 @@
+let mode = 'empty';
+let firstNumberString = '';
+let secondNumberString = '';
+let operator = '';
+let result = 0;
+
 function add(num1, num2) {
     return num1 + num2;
 }
@@ -27,19 +33,108 @@ function operate(operator, num1, num2) {
     }
 }
 
-const buttonContainer = document.querySelector('.buttonContainer')
+// function updateNumberString(text) {
+//     if (operated) {
+//         if (/\d/.test(text)) {
+//             secondNumberString += text;
+//             numberString.textContent += text;
+//         }
+//     } else {
+//         if (/\d/.test(text)) {
+//             firstNumberString += text;
+//             numberString.textContent += text;
+//         } else if (/[\+\/\*]/.test(text) || text == '-') {
+//             operated = true;
+//             operator = text;
+//             numberString.textContent += ` ${text} `
+//         }
+//     }
+//     console.log(operator, firstNumberString, secondNumberString)
+// }
 
+function updateNumberString(text) {
+    switch (mode) {
+        case 'empty':
+            if (/[1-9]/.test(text)) {
+                firstNumberString += text;
+                numberString.textContent = text;
+                mode = 'number1';
+            } else if (/[\+\/\*]/.test(text) || text =='-' && numberString.textContent) {
+                firstNumberString = numberString.textContent;
+                numberString.textContent += text;
+                operator = text;
+                mode = 'operator'
+            }
+            break
+        case 'number1':
+            if (/\d/.test(text)) {
+                firstNumberString += text;
+                numberString.textContent += text;
+            } else if (/[\+\/\*]/.test(text) || text =='-') {
+                operator = text;
+                numberString.textContent += text;
+                mode = 'operator';
+            }
+            break
+        case 'operator':
+            if (/[1-9]/.test(text)) {
+                secondNumberString += text;
+                numberString.textContent += text;
+                mode = 'number2'
+                result = operate(operator, +firstNumberString, +secondNumberString);
+                evaluation.textContent = result
+            } else if (/[\+\/\*]/.test(text) || text =='-') {
+                operator = text;
+                numberString.textContent = numberString.textContent.slice(0,-1);
+                numberString.textContent += text;
+            }
+            break
+        case 'number2':
+            if (/\d/.test(text)) {
+                secondNumberString += text;
+                numberString.textContent += text;
+                result = operate(operator, +firstNumberString, +secondNumberString);
+                evaluation.textContent = result;
+            } else if (/[\+\/\*]/.test(text) || text =='-') {
+                operator = text;
+                numberString.textContent += text;
+                firstNumberString = String(result);
+                secondNumberString = '';
+                mode = 'operator';
+            } else if (text == '=') {
+                numberString.textContent = String(result);
+                firstNumberString = '';
+                secondNumberString = '';
+                evaluation.textContent = '';
+                mode = 'empty'
 
-const buttons = ['C', '()', '%', '/',
+            }
+    }
+    console.log(firstNumberString, operator, secondNumberString, result)
+}
+
+const buttonContainer = document.querySelector('.buttonContainer');
+
+const evaluation = document.querySelector('.evaluation');
+
+const operatorString = document.querySelector('.operatorString');
+
+let numberString = document.querySelector('.numString');
+
+const buttonList = ['C', '()', '%', '/',
                  '7', '8', '9', '*',
                  '4', '5', '6', '-',
                  '1', '2', '3', '+',
                  '+/-', '0', '.', '=']
 
-for (let x in buttons) {
+for (let x in buttonList) {
     const button = document.createElement('button');
-    button.textContent = `${buttons[x]}`;
-    button.classList.add(`${buttons[x]}`);
+    button.textContent = `${buttonList[x]}`;
+    button.classList.add(`${buttonList[x]}`);
     button.classList.add('button');
     buttonContainer.append(button);
 }
+
+const buttons = document.querySelectorAll('.button');
+
+buttons.forEach(button => button.addEventListener('click', (e) => updateNumberString(e.target.textContent)));
